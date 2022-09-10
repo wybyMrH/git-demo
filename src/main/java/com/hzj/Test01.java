061227b68b8b50db6d276687fab12eff9148adc5
Mr.H <2627885590@qq.com>
2022-09-10 10:08:16 +0800

    IDEA hot-fix first commit 22-9-10

--- src/main/java/com/hzj/Test01.java
+++ src/main/java/com/hzj/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello World 01");
         System.out.println("Hello 2");
+        System.out.println("Hello by hot-fix");
     }
 }
