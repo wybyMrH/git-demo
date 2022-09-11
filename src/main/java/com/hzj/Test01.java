324cae546ba3c208809949eb97ec4f2c61e1fa9a
Mr.H <2627885590@qq.com>
2022-09-11 16:41:10 +0800

    master gitee first commit 9-10

--- src/main/java/com/hzj/Test01.java
+++ src/main/java/com/hzj/Test01.java
@@ -8,5 +8,6 @@ public class Test01 {
         System.out.println("second modify by hot-fix");
         System.out.println("Hello by master1");
         System.out.println("Edit by github");
+        System.out.println("push to Gitee");
     }
 }
